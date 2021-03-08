6450cdf9ef08f76720ae811524fbb833fe5c7467
Cristina García <66566180+cgalopez@users.noreply.github.com>
2021-03-08 20:05:32 +0100

    master branch motif message changed

--- seqClass.py
+++ seqClass.py
@@ -27,7 +27,7 @@ if args.motif:
   args.motif = args.motif.upper()
   print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
   if re.match(args.motif, args.seq):
-    print("FOUND")
+    print("FOUND in sequence")
   else:
     print("NOT FOUND")
 
